68f72becc7713fa81d77357f2179a52cb8c52c10
NJOROMBE <sxnjorombe@github.com>
2016-09-20 21:00:12 -0500

    slider

---
+++ assets/js/slider.js
@@ -0,0 +1,37 @@
+var currentIndex = 0,
+  items = $('.slides-img'),
+  itemAmt = items.length;
+
+cycleItems();
+
+function cycleItems() {
+  var item = $('.slides-img').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
+
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 3000);
+
+function nextSlide() {
+  clearInterval(autoSlide);
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}
+
+function prevSlide() {
+  clearInterval(autoSlide);
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
+}
